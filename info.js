2cbbd9544102187cab38cb1c95266b081f47ec7b
twaran123 <42232472+twaran123@users.noreply.github.com>
2023-04-26 12:53:21 -0400

    Create info.js

---
+++ info.js
@@ -0,0 +1,332 @@
+{
+  "user": [
+    {
+      "userId": 1,
+      "name": "Twaran Sahai",
+      "designation": "Web Developer (Full Stack Developer)",
+      "photo": "https://media.licdn.com/dms/image/D5603AQF0fj6puMEeAg/profile-displayphoto-shrink_400_400/0/1676495794707?e=1683158400&v=beta&t=i6XlCVtCxpcwxZAbRmavjNamYRi80SoElEdGzpx9rbc",
+      "phoneNo": "6478653460",
+      "latestJob": "Frelancer",
+      "address": "Conestoga College",
+      "gmailId": "twaransahai28@gmail.com",
+      "linkdinId": "https://www.linkedin.com/in/twaran-sahai-1232ab16b/",
+      "researchGate": "https://github.com/twaran1998"
+    }
+  ],
+  "qualification": [
+    {
+      "qualification_id": 1,
+      "description": "Ambitious, resourceful & results-driven Web Developer with a Postgraduate Diploma in Web Development from Conestoga College, Ontario, Canada, and a Bachelor of Engineering in Computer Science Engineering from Jaypee University of Engineering and Technology, India. Prolific career of over 6 months experience designing and developing sites from concept to rollout. Brings detailed understanding of various programming languages to webpage planning, development, and maintenance. A seasoned expert in languages such as HTML, PHP, ES6, CSS3, React Js, Node JS, and Express Js, WordPress. Takes on multiple simultaneous tasks with excellent time management abilities and a resourceful approach. Offer exceptional communication, organizational, and problem-solving skills. Possess strong ability to work independently and collaboratively in a robust team concept environment. I am seeking a web developer position with the opportunity to grow and contribute to the company's success through my innovative, creative skills.",
+      "stat_one": " Highly adept at front-end web development using JavaScript, CSS, HTML, Bootstrap, React Js, JavaScript, jQuery, CSS,RestAPI",
+      "stat_two": " Outstanding knowledge of Backend Development, including PHP, Node Js, Express, MongoDB, and MySQL.",
+      "stat_three": " Strong skills in developing custom WordPress themes from scratch using ACF, WooCommerce, Custom Post Type, Action & Filter Hooks",
+      "stat_four": " Demonstrating excellence in converting XD/Photoshop files to the Elementor website. "
+    }
+  ],
+  "education": [
+    {
+      "collegeName": "JAYPEE UNIVERSITY OF ENGINEERING AND TECHNOLOGY",
+      "degree": "COMPUTER SCIENCE ENGINEERING(B.Tech)",
+      "year": "2017-2021",
+      "img": "https://media.biltrax.com/wp-content/uploads/2022/07/7f2fb4d21b156c5746011b69052701b4.jpg"
+    },
+    {
+      "collegeName": "CONSESTOGA COLLEGE ",
+      "degree": "WEB DEVELOPMENT (PG DIPLOMA)",
+      "year": "2021-2023",
+      "img": "https://pbs.twimg.com/profile_images/1146156951207907331/tDtLzN5s_400x400.jpg"
+    }
+  ],
+  "professionalSkills": [
+    {
+      "skillName": "MERN STACK ",
+      "skillNeed": "MongoDB,Express,ReactJs,NodeJs"
+    },
+    {
+      "skillName": " Web Development Skills",
+      "skillNeed": "HTML5,CSS3,JavaScript,AJAX,JSON,Rest API ,API Documentation"
+    },
+    {
+      "skillName": "Programming Language",
+      "skillNeed": "C# , PHP ,.NET core "
+    },
+    {
+      "skillName": "Database",
+      "skillNeed": "MySQL "
+    },
+    {
+      "skillName": "Version Control System ",
+      "skillNeed": "Git and Github , GitLab"
+    },
+    {
+      "skillName": "Wordpress",
+      "skillNeed": "WordPress Theme Development,ACF, WooCommerce"
+    },
+    {
+      "skillName": "Photoshop",
+      "skillNeed": "Adobe XD"
+    }
+  ],
+  "CoursesandTraning": [
+    {
+      "name": "THE WEB DEVELOPMENT BOOTCAMP ",
+      "date": "July 2020"
+    },
+    {
+      "name": "THE ULTIMATE MySQL Bootcamp:Go from SQL Begineer to Expert course",
+      "date": "August 2020"
+    },
+    {
+      "name": "THE MODERN JAVASCRIPT BOOTCAMP COURSE",
+      "date": "August 2020"
+    },
+    {
+      "name": "REACT DEVLOPMENT BOOTCAMP",
+      "date": "Jan 2023"
+    },
+    {
+      "name": "GIT AND GITHUB",
+      "date": "Jan 2023"
+    }
+  ],
+  "oral": [
+    {
+      "name": "15th Annual DNACON: Neurosciences-in the evolving era of technology and teamwork",
+      "date": "16th February 2013",
+      "location": "Delhi Neurological Association"
+    },
+    {
+      "name": "3rd International Conference on Eye tracking, Visual cognition and Emotion (ETVCE 2012)",
+      "date": "25th & 26th October 2012",
+      "location": " Lisbon, Portugal"
+    }
+  ],
+  "posters": [
+    {
+      "name": "Society for Neuroscience 2022 annual meeting",
+      "date": "12-16 November",
+      "location": "San Diego, California, United States"
+    },
+    {
+      "name": "Experimental Biology 2018 meeting",
+      "date": "21-25 April 2018",
+      "location": "San Diego, California, United States"
+    },
+    {
+      "name": "National Center for Toxicological Research Science Day",
+      "date": "April 2018",
+      "location": "Food and Drug administration (FDA)"
+    },
+    {
+      "name": "Gordon Research Conference on Eye Movements",
+      "date": "7- 12 July 2013",
+      "location": "Stonehill College in Easton MA United States"
+    },
+    {
+      "name": "57th Annual conference of Physiologists and Pharmocologists",
+      "date": "Stonehill College in Easton MA United States",
+      "location": "India, IJPP"
+    },
+    {
+      "name": "International update of Basic and Clinical Neuroscience advances and XXIV Annual Conference of Indian Academy of Neurosciences",
+      "date": " December 17-20,2006",
+      "location": " ITRC, Lucknow, India"
+    },
+    {
+      "name": "Third NBRC International Conference, (sponsored by Nature Neuroscience)",
+      "date": " December 11-13, 2006",
+      "location": " Delhi, India"
+    },
+    {
+      "name": "International Symposium",
+      "date": " 15-17 December, 2003",
+      "location": " NBRC, India"
+    }
+  ],
+  "publications": [
+    {
+      "pub_id": 1,
+      "title": "Quantitative Neurotoxicology: Potential role of Artificial Intelligence/Deep learning approach.Journal of Applied Toxicology",
+      "date": "2020 Nov 02",
+      "url": "https://analyticalsciencejournals.onlinelibrary.wiley.com/doi/abs/10.1002/jat.4098",
+      "Author": ["Srivastava A", "Hanig JP"],
+      "button": "DOI:10.1002/jat.4098"
+    },
+    {
+      "pub_id": 2,
+      "title": "Quantitative Neurotoxicology: An assessment of the neurotoxic profile of Kainic Acid.International Journal of Toxicology",
+      "date": "2020 May 29",
+      "url": "https://pubmed.ncbi.nlm.nih.gov/32468881/",
+      "Author": [
+        "Srivastava A",
+        "Liachenko S",
+        "Sarkar S",
+        "Paule M",
+        "Negi G",
+        "Pandey J",
+        "Hanig JP"
+      ],
+      "button": "DOI:10.1177/1091581820928497"
+    },
+    {
+      "pub_id": 3,
+      "title": "Saccadic Eye Movements in Patients of Young Onset Parkinson’s Disease: A BOLD fMRI study. Neuro- Opthlamology",
+      "date": "2019",
+      "url": "https://www.tandfonline.com/doi/full/10.1080/01658107.2019.1652656",
+      "Author": ["Srivastava A", "Sharma R", "Goyal V", "Sood SK", "Kumaran S"],
+      "button": "DOI: 10.1080/01658107.2019.1652656"
+    },
+    {
+      "pub_id": 4,
+      "title": "Relationship between saccade and gait. Journal of Movement Disorders",
+      "date": "2018 Sept",
+      "url": "https://pubmed.ncbi.nlm.nih.gov/30086615/",
+      "Author": ["Srivastava A", "Goyal V", "Sood SK", "Sharma R"],
+      "button": "11(3):93-106"
+    },
+    {
+      "pub_id": 5,
+      "title": "Reduced Saccadic velocity in Patients with Young Onset Parkinson’s Disease. Neurology, Psychiatry and Brain Research, Volume 27",
+      "date": "February 2018",
+      "url": "https://pubmed.ncbi.nlm.nih.gov/30086615/",
+      "Author": ["Srivastava A", "Goyal V", "Sood SK", "Sharma R"],
+      "button": "doi: 10.14802/jmd.18018"
+    }
+  ],
+
+  "HonorAward": [
+    {
+      "award": "15 % Scholarship in Web Development (Pg Diploma)",
+      "location": "Conestoga College ,Canada",
+      "year": "Jan 2022"
+    },
+    {
+      "award": "25% Scholarship in Computer Science Engineering (B.Tech)",
+      "location": "Jaypee University of Engineering and Technology, India",
+      "year": "July 2017"
+    }
+  ],
+  "PeerReview": [
+    {
+      "experience": "Peer Reviewed for the journals- Toxicologic Pathology"
+    },
+    {
+      "experience": "Brain sciences"
+    },
+    {
+      "experience": "IJERPH"
+    },
+    {
+      "experience": "Annals of eurosciences"
+    },
+    {
+      "experience": "Annals of Indian Academy of Neurology"
+    }
+  ],
+  "pubications": [
+    {
+      "pub_id": 1,
+      "title": "Quantitative Neurotoxicology: Potential role of Artificial Intelligence/Deep learning approach.Journal of Applied Toxicology.",
+      "date": "2020-11-02",
+      "url": "https://doi.org/10.1002/jat.4098",
+      "Author": ["Srivastava A, Hanig JP"],
+      "button": "Read more"
+    },
+    {
+      "pub_id": 2,
+      "title": "Quantitative Neurotoxicology: An assessment of the neurotoxic profile of Kainic Acid.Journal of Applied Toxicology",
+      "date": "2020-05-29",
+      "url": "https://doi.org/10.1177/1091581820928497",
+      "Author": [
+        "Srivastava A, Liachenko S, Sarkar S, Paule M, Negi G, Pandey J, Hanig J"
+      ],
+      "button": "Read more"
+    },
+    {
+      "pub_id": 3,
+      "title": "Saccadic Eye Movements in Patients of Young Onset Parkinson’s Disease: A BOLD fMRI study,Neuro- Opthlamology",
+      "date": "2019",
+      "url": "https://doi.org/10.1080/01658107.2019.1652656",
+      "Author": ["Srivastava A, Sharma R, Goyal V, Sood SK, Kumaran S"],
+      "button": "Read more"
+    },
+    {
+      "pub_id": 4,
+      "title": "Relationship between saccade and gait.Journal of Movement Disorders",
+      "date": "2018-09",
+      "url": "https://pubmed.ncbi.nlm.nih.gov/30086615/",
+      "Author": ["Srivastava A, Ahmad OF, Pacia CM, Hallett M, Lungu C"],
+      "button": "Read more"
+    },
+    {
+      "pub_id": 5,
+      "title": "Reduced Saccadic velocity in Patients with Young Onset Parkinson’s Disease. Neurology, Psychiatry and Brain Research",
+      "date": "2018-02",
+      "url": "https://doi.org/10.14802/jmd.18018",
+      "Author": ["Srivastava A, Goyal V, Sood SK, Sharma R"],
+      "button": "Read more"
+    },
+    {
+      "pub_id": 6,
+      "title": "Cognition and control of saccadic system. In book P. Gamito, & P. Rosa (Eds.). I see you, you see me: Inferring cognitive and emotional processesfrom gazing behaviour. Newcastle-upon-Tyne: Cambridge Scholars Publishing",
+      "date": "",
+      "url": "https://www.cambridgescholars.com/resources/pdfs/978-1-4438-5460-3-sample.pdf",
+      "Author": ["Srivastava A, Goyal V, Sood SK, Sharma R"],
+      "button": "Read more"
+    },
+    {
+      "pub_id": 7,
+      "title": "Saccadic eye movements in Parkinson's Disease.Indian Journal of Ophthalmology",
+      "date": "2014-05",
+      "url": "https://doi.org/10.4103/0301-4738.133482",
+      "Author": ["Srivastava A, Sood SK, Sharma R, Goyal V"],
+      "button": "Read more"
+    },
+    {
+      "pub_id": 8,
+      "title": "Mechanism of selective attention: fMRI study of face-word stroop task performance. Indian Journal of Physiology and Pharmacology",
+      "date": "2013",
+      "url": "https://pubmed.ncbi.nlm.nih.gov/24968573/",
+      "Author": ["Vivekananth S, Sood SK, Kumaran S, Srivastava A"],
+      "button": "Read more"
+    }
+  ],
+  "workExperience": [
+    {
+      "Job_id": 1,
+      "Job_address": "Ohio USA",
+      "Job_date": "Dec 2022- Feb 2023",
+      "Job_stat_one": "Designed and developed a responsive website with an elegant design, ensuring that it had responsive components and the scope to alter data in the future as per the client's requirements.",
+      "Job_stat_two": "Utilized Material UI and React JS for the front-end development of the website",
+      "Job_stat_three": "Implemented Rest APIs to transmit data from client and server using JSON.",
+      "Job_stat_four": "Collaborated with the client to gather and understand their requirements and provided suggestions and recommendations based on their needs.",
+      "Job_stat_five": "Ensured website is optimized for search engines and had a fast-loading time.",
+      "Job_designation": "Freelance ( Web Developer )",
+      "url": "https://pbs.twimg.com/profile_images/929088511382343682/regsaLAR_400x400.jpg"
+    },
+    {
+      "Job_id": 2,
+      "Job_address": "INDIA",
+      "Job_date": "Nov 2022- April 2023",
+      "Job_stat_one": "Fixed JavaScript and CSS errors of existing website.",
+      "Job_stat_two": "Designed and developed customer and Admin pages using Material UI.",
+      "Job_stat_three": "Worked on Form Validations for Admin and Customer portals.",
+      "Job_stat_four": "Worked in Agile work Environment to deliver the requirements.",
+      "Job_stat_five": "Worked on pdf generator for invoice.",
+      "Job_designation": "Freelance (Full Stack Developer)",
+      "url": "https://pbs.twimg.com/profile_images/929088511382343682/regsaLAR_400x400.jpg"
+    },
+    {
+      "Job_id": 3,
+      "Job_address": "INDIA",
+      "Job_date": "Jan 2020- March 2020",
+      "Job_stat_one": "Developed custom web applications using React and RESTful APIs, creating dynamic and user-friendly interfaces.",
+      "Job_stat_two": "Utilized React Router to create complex routing and navigation systems for multi-page applications.",
+      "Job_stat_three": "Collaborated with UI/UX designers to create responsive designs and optimize user experience.",
+      "Job_stat_four": "Utilized Git and GitHub for version control and collaborated with team members using Agile methodologies. Planned, created, and maintained websites for clients.",
+      "Job_stat_five": "Converted XD templates to responsive HTML or Web design.",
+      "Job_designation": "Freelance (Web Developer)",
+      "url": "https://pbs.twimg.com/profile_images/929088511382343682/regsaLAR_400x400.jpg"
+    }
+  ]
+}
